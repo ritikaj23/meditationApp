dceb670f406da1b12daf10c1ca762d9ae3f1b1ff
ritikaj23 <ritikaj@skillup.online>
2024-12-05 06:37:28 -0500

    modified requirements.txt

--- app/login.js
+++ app/login.js
@@ -1,37 +1,28 @@
 import React, { useState } from "react";
-import {
-  View,
-  SafeAreaView,
-  Alert,
-  TextInput,
-  Text,
-  TouchableOpacity
-} from "react-native";
+import { View, SafeAreaView, Image, Alert, Text, TextInput, TouchableOpacity } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Stack, useRouter } from "expo-router";
-import { COLORS } from "../constants";
+import { COLORS, icons, SHADOWS } from "../constants";
 
 const Login = () => {
-  // State Initialization
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
 
-  // Handle Login Function
+  // Login handler
   const handleLogin = async () => {
     if (!email || !password) {
       Alert.alert("Validation Error", "Please fill in all fields.");
       return;
     }
-
+    
     const userDetails = { email, password, token: "sample-token" };
-
     console.log('userDetails', userDetails);
 
     try {
-      const detailsDatafromSignup = await AsyncStorage.getItem("userDetails");
-      if (detailsDatafromSignup) {
-        const parsedDetails = JSON.parse(detailsDatafromSignup);
+      const detailsDataFromSignup = await AsyncStorage.getItem("userDetails");
+      if (detailsDataFromSignup) {
+        const parsedDetails = JSON.parse(detailsDataFromSignup);
         if (userDetails.email === parsedDetails.email && userDetails.password === parsedDetails.password) {
           router.push("/home");
         } else {
@@ -42,11 +33,12 @@ const Login = () => {
       }
     } catch (error) {
       console.error("Error accessing AsyncStorage", error);
+      Alert.alert("Error", "An error occurred while logging in.");
     }
   };
 
   return (
-    <>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Stack.Screen
         options={{
           headerStyle: { backgroundColor: COLORS.lightWhite },
@@ -55,68 +47,89 @@ const Login = () => {
           headerTitle: "",
         }}
       />
-      <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
-        <View style={{ padding: 20 }} testID="loginContainer">
-          <View style={{ marginTop: 30 }} testID="formData">
-            <View style={{ marginBottom: 10 }} testID="email">
-              <TextInput
-                style={{
-                  borderColor: "#ccc",
-                  borderWidth: 1,
-                  padding: 10,
-                  borderRadius: 5,
-                  marginBottom: 10,
-                }}
-                value={email}
-                onChangeText={setEmail}
-                placeholder="Email"
-              />
-            </View>
-            <View style={{ marginBottom: 20 }} testID="password">
-              <TextInput
-                style={{
-                  borderColor: "#ccc",
-                  borderWidth: 1,
-                  padding: 10,
-                  borderRadius: 5,
-                }}
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry={true}
-                placeholder="Password"
-              />
-            </View>
-            <TouchableOpacity
+      <View style={{ padding: 20 }}>
+        <View
+          style={{
+            padding: 20,
+            marginLeft: "auto",
+            marginRight: "auto",
+            backgroundColor: "#f0f0f0",
+            borderRadius: 50,
+            height: 90,
+            ...SHADOWS.medium,
+            shadowColor: COLORS.white,
+          }}
+        >
+          <Image
+            source={icons.menu}
+            style={{
+              width: 50,
+              height: 50,
+              marginBottom: 20,
+            }}
+          />
+        </View>
+        {/* Form Component */}
+        <View style={{ marginTop: 20 }}>
+          <View style={{ marginBottom: 20 }}>
+            <TextInput
               style={{
-                backgroundColor: COLORS.primary,
-                padding: 15,
+                borderWidth: 1,
+                borderColor: "#ccc",
+                padding: 10,
                 borderRadius: 5,
-                alignItems: "center",
                 marginBottom: 10,
               }}
-              onPress={handleLogin}
-              testID="handleLogin"
-            >
-              <Text style={{ color: "#fff", fontWeight: "bold" }}>Login</Text>
-            </TouchableOpacity>
-            <View
+              value={email}
+              onChangeText={setEmail}
+              placeholder="Email"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+            <TextInput
               style={{
-                flexDirection: "row",
-                justifyContent: "center",
-                alignItems: "center",
-                marginTop: 5,
+                borderWidth: 1,
+                borderColor: "#ccc",
+                padding: 10,
+                borderRadius: 5,
+                marginBottom: 10,
               }}
-              testID="textData"
-            >
-              <Text style={{ marginRight: 5 }}>Don't have an account?</Text>
-              <TouchableOpacity onPress={() => router.push("/signup")}>
-                <Text style={{ color: "blue" }}>Sign Up</Text>
-              </TouchableOpacity>
-            </View>
+              value={password}
+              secureTextEntry={true}
+              onChangeText={setPassword}
+              placeholder="Password"
+            />
           </View>
+          <TouchableOpacity
+            style={{
+              backgroundColor: COLORS.primary,
+              padding: 15,
+              borderRadius: 5,
+              alignItems: "center",
+            }}
+            onPress={handleLogin}
+          >
+            <Text style={{ color: "#fff", fontWeight: "bold" }}>Login</Text>
+          </TouchableOpacity>
         </View>
-      </SafeAreaView>
-    </>
+        {/* Additional Options */}
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: 10,
+          }}
+        >
+          <Text style={{ marginRight: 5 }}>
+            Don't have an account?
+          </Text>
+          <TouchableOpacity onPress={() => router.push("/signup")}>
+            <Text style={{ color: "blue" }}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
